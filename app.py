6862ec258f08555c850e369460d6915fa50db489
Damilola Olatayo <77675650+Boss2021-eng@users.noreply.github.com>
2026-08-08 17:48:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from PIL import Image
 import base64
 from io import BytesIO
 
-st.write("OpenCV version:", cv2.__version__)
-
 # ─────────────────────────────────────────────
 #  PAGE CONFIG
 # ─────────────────────────────────────────────
