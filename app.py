225b77a748b2814ac85e265a276248c22b3a96ab
Damilola Olatayo <77675650+Boss2021-eng@users.noreply.github.com>
2026-08-08 17:36:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 # import cv2
 # from ultralytics import YOLO
 
-st.write("OpenCV:", cv2.__version__)
+# st.write("OpenCV:", cv2.__version__)
 st.write("YOLO imported successfully")
 
-model = YOLO("your_model.pt")
+# model = YOLO("your_model.pt")
 
-st.write("YOLO model loaded successfully")
+# st.write("YOLO model loaded successfully")
